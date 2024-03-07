3955340530632cdf3b2d34f003b73853e73653e4
Zaghdoudi mohamed <mohamed.zaghdoudi@codeonce.fr>
2024-03-07 12:03:56 +0100

    develop product component

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductapiService } from 'src/app/services/productapi.service';
 
 @Component({
   selector: 'app-products',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
+
+  selectedItem: string = '';
+  public productList : any ;
+  public filterCategory : any;
+  searchKey:string ="";
+  constructor(private api : ProductapiService) { }
+
+  ngOnInit(): void {
+    this.api.getProduct()
+    .subscribe(res=>{
+      this.productList = res;
+      this.filterCategory = res;
+      this.productList.forEach((a:any) => {
+        if(a.category ==="women's clothing" || a.category ==="men's clothing"){
+          a.category ="fashion"
+        }
+        Object.assign(a,{quantity:1,total:a.price});
+      });
+      console.log(this.productList)
+    });
+
+
+    
+  }
+
 
 }
--- src/app/services/productapi.service.ts
+++ src/app/services/productapi.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductapiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // fetch products from the API
+  getProduct() {
+    return this.http.get<any>("https://fakestoreapi.com/products")
+      .pipe(map((res: any) => {
+        return res;
+      }))
+  }
 }
